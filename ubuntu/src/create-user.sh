eb89df2e6e6452deea36732264dcc721a50ebfdb
Denis Denisov <denji0k@gmail.com>
2020-11-04 03:27:32 +0200

    Added a delay expect
    Without which the user may not have time to respond by pressing ENTER(\r).
    Perhaps the time should be increased and removed in some places where it is not expected, you should test more.

--- ubuntu/src/create-user.sh
+++ ubuntu/src/create-user.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/expect
 eval spawn /opt/postal/bin/postal make-user
 expect "E-Mail"
+sleep 1
 send "$env(POSTAL_EMAIL)\n"
 expect "First Name"
+sleep 1
 send "$env(POSTAL_FNAME)\n"
 expect "Last Name"
+sleep 1
 send "$env(POSTAL_LNAME)\n"
 expect "Password"
+sleep 1
 send "$env(POSTAL_PASSWORD)\n"
 interact
