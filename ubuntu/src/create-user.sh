277ca32367a7524e336ec0b4c94223060993608e
ILoveYaToo <dev@iloveyatoo.com>
2021-01-22 05:25:50 -0500

    update user creation tool

--- ubuntu/src/create-user.sh
+++ ubuntu/src/create-user.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/expect
 eval spawn /opt/postal/bin/postal make-user
 expect "E-Mail"
-send "$env(POSTAL_EMAIL)\n"
+send "$env(POSTAL_EMAIL)\r"
 expect "First Name"
-send "$env(POSTAL_FNAME)\n"
+send "$env(POSTAL_FNAME)\r"
 expect "Last Name"
-send "$env(POSTAL_LNAME)\n"
+send "$env(POSTAL_LNAME)\r"
 expect "Password"
-send "$env(POSTAL_PASSWORD)\n"
-send "\n"
+sleep 5
+send "$env(POSTAL_PASSWORD)\r"
+send "\r"
 interact
